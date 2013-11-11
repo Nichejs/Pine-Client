357d7b60b987f466a395563c2109ba905632856b
Nicolas_Lozano_Arguelles <nlozanoarguelles@gmail.com>
2013-11-11 11:52:06 +0100

    Fixing rename to pine

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
  * 
  * License: GNU GENERAL PUBLIC LICENSE
  */
-define(["jquery", "open_rpg", "chat", "map", "tree", "character", "socket"], function($, OpenRPG, ChatOpenRPG, MapOpenRPG, Tree, Character, io){
+define(["jquery", "main", "chat", "map", "tree", "character", "socket"], function($, Main, Chat, Map, Tree, Character, io){
 	
 	var App = {
 		lastTimestamp : 0,	// Used for measuring ping
@@ -21,13 +21,13 @@ define(["jquery", "open_rpg", "chat", "map", "tree", "character", "socket"], fun
 	 * @param {Object} Size of the container in {w:_,h:_} format 
 	 */
 	App.init = function(gameContainer, size){
-		OpenRPG.container = gameContainer;
-		OpenRPG.size = size;
+		Main.container = gameContainer;
+		Main.size = size;
 		
-		if(!OpenRPG.logged()){
+		if(!Main.logged()){
 			App.displayLogin();
 		}else{
-			OpenRPG.start();
+			Main.start();
 		}
 	};
 	
@@ -35,7 +35,7 @@ define(["jquery", "open_rpg", "chat", "map", "tree", "character", "socket"], fun
 	 * Displays a login box and handles its events. 
 	 */
 	App.displayLogin = function(){
-		$(OpenRPG.container).append('<h2>Login:</h2><form action="/" method="post" id="loginForm">Usuario: <input type="text" name="user" id="username" /> Contraseña: <input type="password" name="pass" id="pass" /> <input type="submit" value="Login" /><p>Not a user? <a href="/register.html">Register!</a></p></form>');
+		$(Main.container).append('<h2>Login:</h2><form action="/" method="post" id="loginForm">Usuario: <input type="text" name="user" id="username" /> Contraseña: <input type="password" name="pass" id="pass" /> <input type="submit" value="Login" /><p>Not a user? <a href="/register.html">Register!</a></p></form>');
 		
 		// Process the form
 		$('#loginForm').submit(function(event){
@@ -55,24 +55,24 @@ define(["jquery", "open_rpg", "chat", "map", "tree", "character", "socket"], fun
 	App.socketStart = function(user, pass){
 		console.log("Opening socket connection");
 		console.time('Socket connected');
-		OpenRPG.socket = io.connect(OpenRPG.socketHost, { query: "user="+user+"&pass="+pass });
+		Main.socket = io.connect(Main.socketHost, { query: "user="+user+"&pass="+pass });
 		
 		// Launch game
-		OpenRPG.start();
+		Main.start();
 		
-		OpenRPG.socket.on('connect', function () {
+		Main.socket.on('connect', function () {
 			console.timeEnd("Socket connected");
 			
 			// Setup username
-			OpenRPG.user.name = user;
+			Main.user.name = user;
 			
 			// Server notifications
-			OpenRPG.socket.emit('subscribe', 'server');
+			Main.socket.emit('subscribe', 'server');
 			// Other users position
-			OpenRPG.socket.emit('subscribe', 'position');
+			Main.socket.emit('subscribe', 'position');
 			
 			// Setup map
-		 	MapOpenRPG.init();
+		 	Map.init();
 		 	
 		 	// Draw tree
 		 	Tree.newTree(-150,-120,0,80);
@@ -91,62 +91,62 @@ define(["jquery", "open_rpg", "chat", "map", "tree", "character", "socket"], fun
 			App.character = Character.newCharacter({
 				position: {x:110,y:0,z:0},
 				movable: true,
-				name: OpenRPG.user.name
+				name: Main.user.name
 			});
 		 	
 		 	// Setup chat
-		 	ChatOpenRPG.init($('#chatOut').get(0), $('#chatIn').get(0));
+		 	Chat.init($('#chatOut').get(0), $('#chatIn').get(0));
 		});
 		
 		// Process ping messages
-		OpenRPG.socket.on('ping', function (data) {
+		Main.socket.on('ping', function (data) {
 			// I divide by two to get one way only
 			var ping = ((new Date()).getTime() - data.timestamp)/2;
 			$('#server').html("Ping: "+ping+"ms");
 		});
 		
 		// Process server messages
-		OpenRPG.socket.on('message', function (data) {
+		Main.socket.on('message', function (data) {
 			if(data.room !== 'server') return;
 			
 			if(data.type == 'connect'){
 				// User connected
 				// We will allow only one connection, so if the user
 				// is the same, bye bye!
-				if(OpenRPG.user.name.toLowerCase() == data.user.toLowerCase()){
+				if(Main.user.name.toLowerCase() == data.user.toLowerCase()){
 					// They are the same, bye bye to one of them
-					ChatOpenRPG.displayMessage(data.user+' logged in twice', 'server');
-					OpenRPG.socket.disconnect();
+					Chat.displayMessage(data.user+' logged in twice', 'server');
+					Main.socket.disconnect();
 					window.location = '/';
 				}else{
 					// Display message on chat:
-					ChatOpenRPG.displayMessage(data.user+' joined', 'server');
+					Chat.displayMessage(data.user+' joined', 'server');
 				}
 				
 				// Notify of our position
-				OpenRPG.socket.emit('send', { room: 'position', position : App.character.centerp });
+				Main.socket.emit('send', { room: 'position', position : App.character.centerp });
 			}
 			
 			if(data.type == 'disconnect'){
 				// User disconnected
 				// Display message on chat:
-				ChatOpenRPG.displayMessage(data.user+' left', 'server');
+				Chat.displayMessage(data.user+' left', 'server');
 				// Remove from sheetengine
 				try{
-					MapOpenRPG.densityMap.remove(App.players[data.user]);
+					Map.densityMap.remove(App.players[data.user]);
 					App.players[data.user].destroy();
 				}catch(e){}
 				// Remove from local cache
 				delete App.players[data.user];
 				// Remove from static queue
-				if(data.user !== OpenRPG.user.name) MapOpenRPG.removeFromStaticQueue(data.user);
+				if(data.user !== Main.user.name) Map.removeFromStaticQueue(data.user);
 				
-				MapOpenRPG.redraw();
+				Map.redraw();
 			}
 		});
 		
 		// Process other players movement
-		OpenRPG.socket.on('update', function (data) {
+		Main.socket.on('update', function (data) {
 			if(App.players[data.user]){
 				// Player is being tracked
 				App.players[data.user].moveTo(data.position);
@@ -162,15 +162,15 @@ define(["jquery", "open_rpg", "chat", "map", "tree", "character", "socket"], fun
 		
 		var pingInterval = setInterval(function(){
 			// Ping measure
-			OpenRPG.socket.emit('ping', { timestamp: (new Date()).getTime() });
+			Main.socket.emit('ping', { timestamp: (new Date()).getTime() });
 		},1000);
 		
 		// Process server messages
-		OpenRPG.socket.on('usersOnline', function (data) {
+		Main.socket.on('usersOnline', function (data) {
 			$('#usersOnline').html(data.count+' online');
 		});
 		
-		OpenRPG.socket.on('error', function (err) {
+		Main.socket.on('error', function (err) {
 			window.location = '/';
 			console.error("Error de conextion: ",err);
 		});
